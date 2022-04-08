ca1c7a42f601675394a3792d8b828e47a7417bd1
argie <argeline0729@yahoo.com>
2022-04-08 10:04:38 -0400

    still working on the codes

---
+++ .github/ISSUE_TEMPLATE/app.js
@@ -0,0 +1,18 @@
+
+//
+const startButton = document.getElementById('start-btn')
+
+startButton.addEventListener('click', startGame)
+
+function StartGame() {
+    console.log('start')
+    startButton.Button.classList.add('hide')
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
